446709e8bba040ccaf7931bb42d16e22c66adbf8
Xolani Mfuko <xmfuko@gmail.com>
2014-08-13 23:55:44 -0700

    edit the spec helper to include the lines spec file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,13 @@
 require 'pg'
 require 'rspec'
 require './lib/stations'
+require './lib/lines'
 
 DB = PG.connect(:dbname => 'train_system_test')
 
 RSpec.configure do |config|
   config.after(:each) do
-<<<<<<< Updated upstream
-=======
     DB.exec("DELETE FROM stations *;")
     DB.exec("DELETE FROM lines *;")
->>>>>>> Stashed changes
   end
 end
