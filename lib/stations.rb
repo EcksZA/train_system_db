aae627119395f9a4cf95831f270a906e8fce4118
Xolani Mfuko <xmfuko@gmail.com>
2014-08-13 22:57:28 -0700

    save the stations objects into the stations array

--- lib/stations.rb
+++ lib/stations.rb
@@ -3,10 +3,20 @@ class Station
 
   def initialize(attributes)
     @name = attributes[:name]
-    @id = attributes[:name]
+    @id = attributes[:id]
   end
 
   def self.all
-    list = []
+    stations_list = []
+    results = DB.exec("SELECT * FROM stations")
+    results.each do |result|
+      stations_list << Station.new({:name => result['name'], :id => result['id'].to_i})
+    end
+    stations_list
+  end
+
+  def save
+    results = DB.exec("INSERT INTO stations (name) VALUES ('#{@name}') RETURNING id;")
+    @id = results.first['id'].to_i
   end
 end
--- spec/stations_spec.rb
+++ spec/stations_spec.rb
@@ -15,4 +15,10 @@ describe Station do
   it "creates an empty array to store the stations" do
     expect(Station.all).to eq []
   end
+
+  it "saves a new station into the stations array" do
+    station_1 = Station.new({:name => "Rosebank"})
+    station_1.save
+    expect(Station.all).to eq [station_1]
+  end
 end
