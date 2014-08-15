074d7f479280a15b4dc38390e55b9cba42966bb9
Xolani Mfuko <xmfuko@gmail.com>
2014-08-14 20:20:45 -0700

    read the values of the object's properties

--- lib/stops.rb
+++ lib/stops.rb
@@ -1,6 +1,10 @@
 class Stops
+  attr_reader :station_id, :line_id
+
   def initialize(attributes)
     @station_id = attributes[:station_id]
     @line_id = attributes[:line_id]
   end
+
+
 end
--- spec/stops_spec.rb
+++ spec/stops_spec.rb
@@ -5,4 +5,10 @@ describe Stops do
     stop_1 = Stops.new({:station_id => 2, :line_id => 3})
     expect(stop_1).to be_an_instance_of Stops
   end
+
+  it "lists out the arguments of the object" do
+    stop_1 = Stops.new({:station_id => 2, :line_id => 3})
+    expect(stop_1.station_id).to eq 2
+    expect(stop_1.line_id).to eq 3
+  end
 end
